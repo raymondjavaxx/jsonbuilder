91b5e5f7df53696e1815cc5ea5d25b401ea29ae6
Ramon Torres <raymondjavaxx@gmail.com>
2012-05-14 16:47:41 +0800

    Added license

--- libraries/jsonbuilder/Exception.php
+++ libraries/jsonbuilder/Exception.php
@@ -1,6 +1,22 @@
 <?php
+/**
+ * JSONBuilder
+ *
+ * Copyright (c) 2012 Ramon Torres
+ *
+ * Licensed under the MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) 2012 Ramon Torres
+ * @license The MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 
 namespace jsonbuilder;
 
+/**
+ * Package exception
+ *
+ * @package jsonbuilder
+ */
 class Exception extends \Exception {
 }
--- libraries/jsonbuilder/JSONBuilder.php
+++ libraries/jsonbuilder/JSONBuilder.php
@@ -1,11 +1,22 @@
 <?php
+/**
+ * JSONBuilder
+ *
+ * Copyright (c) 2012 Ramon Torres
+ *
+ * Licensed under the MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) 2012 Ramon Torres
+ * @license The MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 
 namespace jsonbuilder;
 
 /**
  * Utility class for generating JSON
  *
- * @package default
+ * @package jsonbuilder
  */	
 class JSONBuilder {
 
